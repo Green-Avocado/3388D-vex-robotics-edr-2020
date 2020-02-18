29608340af5f08c02cacc0239ceb43503a5592a6
Green-Avocado <greenavocado@protonmail.com>
2020-02-18 09:33:45 -0700

    Added framework for replay

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,111 @@
 using namespace okapi;
 
 //drive declaration
+/*
 okapi::MotorGroup driveLeft({3_mtr, 4_mtr});
 okapi::MotorGroup driveRight({8_rmtr, 9_rmtr});
 auto drive = ChassisControllerFactory::create(driveLeft, driveRight);
+*/
 
 //motors declaration
+pros::Motor driveLeft1(3, 0);
+pros::Motor driveLeft2(4, 0);
+pros::Motor driveRight1(8, 1);
+pros::Motor driveRight2(9, 1);
 pros::Motor arm(10, 0);
 pros::Motor intakeLeft(1, 1);
 pros::Motor intakeRight(2, 0);
 pros::Motor tray(6, 1);
+
+//controller declarations
+//Controller masterController;
+pros::Controller master (CONTROLLER_MASTER);
+
+ControllerButton trayUpButton(ControllerDigital::L1);
+ControllerButton trayDownButton(ControllerDigital::L2);
+
+ControllerButton armUpButton(ControllerDigital::R1);
+ControllerButton armDownButton(ControllerDigital::R2);
 
 //constants
 #define driveSpeed 0.8
 #define armSpeed 0.75
 #define intakeSpeed 1
 #define traySpeed 0.7
+#define replayFrames 150
+
+//replay memory
+int driveLeftArr[replayFrames];
+int driveRightArr[replayFrames];
+int armArr[replayFrames];
+int intakeArr[replayFrames];
+int trayArr[replayFrames];
+
+//motor functions
+void Fdrive(int x, int y) {
+    if(abs(y + x) > 3)
+    {
+        driveLeft1.move((y + x) / 2 * driveSpeed);
+        driveLeft2.move((y + x) / 2 * driveSpeed);
+        driveRight1.move((y - x) / 2 * driveSpeed);
+        driveRight2.move((y - x) / 2 * driveSpeed);
+    }
+    else
+    {
+        driveLeft1.move(0);
+        driveLeft2.move(0);
+        driveRight1.move(0);
+        driveRight2.move(0);
+    }
+}
+
+void Farm(bool x, bool y) {
+    if(x)
+    {
+        arm.move(127 * armSpeed);
+    }
+    else if(y)
+    {
+        arm.move(-127 * armSpeed);
+    }
+    else
+    {
+        arm.move(0);
+    }
+}
+
+void Fintake(int x) {
+    if(abs(x) > 3)
+    {
+        intakeLeft.move(x * intakeSpeed);
+        intakeRight.move(x * intakeSpeed);
+    }
+    else
+    {
+        intakeLeft.move(0);
+        intakeRight.move(0);
+    }
+}
+
+void Ftray(bool x, bool y) {
+    if(x)
+    {
+        tray.move(127 * traySpeed);
+    }
+    else if(y)
+    {
+        tray.move(-127 * traySpeed);
+    }
+    else
+    {
+        tray.move(0);
+    }
+}
+
+void replay() {
+    for(int i; i < replayFrames; i++) {
+    }
+}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -28,8 +118,8 @@ pros::Motor tray(6, 1);
 void initialize() {
 	pros::lcd::initialize();
 
-    driveLeft.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
-    driveRight.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+    //driveLeft.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+    //driveRight.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
 
     arm.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     intakeLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
@@ -82,16 +172,13 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	Controller masterController;
-    pros::Controller master (CONTROLLER_MASTER);
-
-    ControllerButton trayUpButton(ControllerDigital::L1);
-    ControllerButton trayDownButton(ControllerDigital::L2);
-
-    ControllerButton armUpButton(ControllerDigital::R1);
-    ControllerButton armDownButton(ControllerDigital::R2);
-
 	while (true) {
+        Fdrive(master.get_analog(ANALOG_LEFT_Y), master.get_analog(ANALOG_LEFT_X));
+        Farm(armUpButton.isPressed(), armDownButton.isPressed());
+        Fintake(master.get_analog(ANALOG_RIGHT_Y));
+        Ftray(trayUpButton.isPressed(), trayDownButton.isPressed());
+
+        /* OLD CONTROLS
         //drivetrain arcade movement
         if(abs(master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_LEFT_X)) > 3)
         {
@@ -141,6 +228,7 @@ void opcontrol() {
         {
             tray.move(0);
         }
+        */
         
         pros::delay(20);
 	}
